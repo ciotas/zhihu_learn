076672ba08e6282de54909f0502b630047fcd93b
ciotas <linzikristy@qq.com>
2017-04-25 22:29:16 +0800

    zlz

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::get('question/{question}/follow','QuestionFollowController@follow');
 Route::get('notifications','NotificationsController@index');
 Route::get('notifications/{notification}','NotificationsController@show');
 
+Route::get('avatar','UsersController@avatar');
+
 Route::get('inbox','InboxController@index');
 Route::get('inbox/{dialogId}','InboxController@show');
 Route::post('inbox/{dialogId}/store','InboxController@store');
